626932bbf35e572028527b46a6a78b3430aebd4f
kazuyaTakahashi1988 <kazuya.takahashi.631130@gmail.com>
2023-12-31 21:38:25 +0900

    Revert "TodoItems コンポーネント 修正"
    This reverts commit 34561fecce5c24f5ac6237809a73b42881376d93.

--- src/components/Form/TodoItems.tsx
+++ src/components/Form/TodoItems.tsx
@@ -1,4 +1,4 @@
-import React, { useState, forwardRef } from "react";
+import React, { forwardRef } from "react";
 import styled from "styled-components";
 import { PropsTodoItems } from "../../lib/Props";
 
@@ -17,17 +17,10 @@ export const TodoItemsField: React.ForwardRefRenderFunction<
   const name = rest.name;
   const { checkBoxName, inputName } = itemsName;
   const appendFormat = { [checkBoxName]: false, [inputName]: "" };
-  const [isDisabled, setIsDisabled] = useState(false);
-
-  const onAppend = async () => {
-    setIsDisabled(true);
-    await append(appendFormat);
-    setIsDisabled(false);
-  };
 
   return (
     <Styled className={rest.className} ref={ref}>
-      <Button onClick={() => onAppend()}>追加</Button>
+      <Button onClick={() => append(appendFormat)}>追加</Button>
 
       <ul className="todo-list">
         {fields?.map((field, index) => (
@@ -40,7 +33,6 @@ export const TodoItemsField: React.ForwardRefRenderFunction<
             <Input
               {...rest}
               {...register(`${name}.${index}.${inputName}` as const)}
-              disabled={isDisabled}
               className="input"
             />
             <Button
