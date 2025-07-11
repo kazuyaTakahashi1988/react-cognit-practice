990c6a168efde21c8546f1818abc95db01f820e3
kazuyaTakahashi1988 <kazuya.takahashi.631130@gmail.com>
2025-07-12 02:51:48 +0900

    eslint&prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,36 +1,51 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
-  // parserOptions: {
-  //   ecmaVersion: 'latest',
-  //   sourceType: 'module',
-  //   project: ['./tsconfig. json'],
-  //   tsconfigRootDir: __dirname
-  // },
-  ignorePatterns: ["dist", "node_modules"],
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "plugin:react-hooks/recommended", "plugin:storybook/recommended", "prettier"],
+  env: {
+    browser: true,
+    es2020: true,
+    node: true,
+  },
   parser: "@typescript-eslint/parser",
-  plugins: ["react-refresh", "import"],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: ["./tsconfig.json"],
+    tsconfigRootDir: __dirname,
+  },
+  ignorePatterns: ["dist", "node_modules"],
+  settings: {
+    react: {
+      version: "detect",
+    },
+    "import/resolver": {
+      typescript: {
+        alwaysTryTypes: true, // optional
+        project: "./tsconfig.json", // tsconfigのパスを正しく
+      },
+    },
+  },
+  plugins: ["@typescript-eslint", "react", "react-hooks", "import", "sonarjs"],
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
+    "plugin:react-hooks/recommended",
+    "plugin:sonarjs/recommended",
+    "plugin:react/jsx-runtime",
+    "prettier",
+  ],
   rules: {
-    /* ------------------------------------------------
-      ▽▽▽ プロジェクトごとにLintルールを追加しよう ▽▽▽
-    ------------------------------------------------ */
-    "react-refresh/only-export-components": [
-      "warn",
-      { allowConstantExport: true },
-    ],
-    "no-undef": "off",
     "react/react-in-jsx-scope": "off",
-    "no-use-before-define": 0,
-    // 'no-console': 'error',
-    // 'no-restricted-syntax': [
-    //   'error',
-    //   {
-    //     'selector': 'CallExpression[callee.object.name="console"][callee.property.name!=/^(log|warn|error|info|trace)$/]',
-    //     'message': 'Unexpected property on console object was called'
-    //   }
-    // ]
+    "no-use-before-define": "off",
+    "no-console": "error",
+    "no-undef": "off",
+    "import/default": "off",
+    "import/no-named-as-default": "off",
     quotes: ["error", "double"],
+
+    // import順
     "import/order": [
       "error",
       {
@@ -38,17 +53,25 @@ module.exports = {
           "builtin",
           "external",
           "internal",
-          ["parent", "sibling"],
+          ["parent", "sibling", "index"],
           "object",
           "type",
-          "index",
         ],
-        pathGroupsExcludedImportTypes: ["builtin"],
         "newlines-between": "always",
         alphabetize: {
-          order: "asc"
-        }
-      }
-    ]
+          order: "asc",
+          caseInsensitive: true,
+        },
+        pathGroupsExcludedImportTypes: ["builtin"],
+      },
+    ],
+
+    // 認知的複雑度（Cognitive Complexity）
+    "sonarjs/cognitive-complexity": ["error", 5],
+    complexity: ["error", { max: 5 }], // 関数内の分岐複雑度
+    "max-depth": ["error", 15], // ネストの深さ制限
+    "no-else-return": ["error"], // 不要な else の排除
+    "sonarjs/no-small-switch": ["error"], // 無意味に小さい switch の警告
+    // 認知的複雑度（デフォルトの上限はなし）
   },
-}
+};
