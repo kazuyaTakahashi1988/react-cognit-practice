5adaa24e945d1aecb810e6cb18fb8b6c4cc7e55b
kazuyaTakahashi1988 <57883262+kazuyaTakahashi1988@users.noreply.github.com>
2026-04-01 19:02:08 +0900

    Align Storybook form styles with app global styles

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,7 +1,17 @@
 import type { Preview } from "@storybook/react";
+
+import { GlobalStyle } from "../src/lib/style";
 import "./storybook.css";
 
 const preview: Preview = {
+  decorators: [
+    (Story) => (
+      <>
+        <GlobalStyle />
+        <Story />
+      </>
+    ),
+  ],
   parameters: {
     actions: { argTypesRegex: "^on[A-Z].*" },
     controls: { matchers: { color: /(background|color)$/i, date: /Date$/i } },
