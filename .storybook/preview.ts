f75c431ebcc48795388fbbf903185743c123db98
kazuyaTakahashi1988 <57883262+kazuyaTakahashi1988@users.noreply.github.com>
2026-04-01 19:10:00 +0900

    Fix Storybook preview parse error without JSX in TS file

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,3 +1,4 @@
+import React from "react";
 import type { Preview } from "@storybook/react";
 
 import { GlobalStyle } from "../src/lib/style";
@@ -5,12 +6,13 @@ import "./storybook.css";
 
 const preview: Preview = {
   decorators: [
-    (Story) => (
-      <>
-        <GlobalStyle />
-        <Story />
-      </>
-    ),
+    (Story) =>
+      React.createElement(
+        React.Fragment,
+        null,
+        React.createElement(GlobalStyle),
+        React.createElement(Story),
+      ),
   ],
   parameters: {
     actions: { argTypesRegex: "^on[A-Z].*" },
