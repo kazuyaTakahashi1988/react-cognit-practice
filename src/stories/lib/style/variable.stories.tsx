d77424adf4d1989d0af954a2b1340f5120c14f0e
kazuyaTakahashi1988 <kazuya.takahashi.631130@gmail.com>
2026-04-07 12:17:54 +0900

    修正

--- src/stories/lib/style/variable.stories.tsx
+++ src/stories/lib/style/variable.stories.tsx
@@ -84,4 +84,4 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Colors: Story = { render: () => <ColorAssetList /> };
+export const Color: Story = { render: () => <ColorAssetList /> };
